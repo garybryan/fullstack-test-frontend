ade8f0ca291863218850a70fb83aec56c3aeaad1
Gary Bryan <gary.bryan@mercurytide.co.uk>
2019-05-06 13:34:59 +0100

    Search for real

--- src/App.js
+++ src/App.js
@@ -4,24 +4,32 @@ import './css/App.css';
 import SearchBar from './components/searchbar'
 import SearchResults from './components/searchresults'
 
-const TEST_RESULTS = [
-  {'name': 'Edinburgh', 'postcode': 'EH8 9BH'},
-  {'name': 'Leith', 'postcode': 'EH6 6AY'},
-];
+const API_URL = "http://localhost:8000/"
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      results: []
+      results: [],
+      isLoading: false
     }
     this.searchInput = React.createRef('input');
   }
   search = (event) => {
     const query = this.searchInput.current.value;
+
     event.preventDefault();
-    const results = TEST_RESULTS
-    this.setState({results});
+    this.setState({isLoading: true});
+
+    fetch(`${API_URL}stores?search=${query}`).then(
+      res => res.json()
+    ).then((results) => {
+      this.setState({results});
+    }).catch((error) => {
+      alert("API error\n" + error);
+    }).finally(() => {
+      this.setState({isLoading: false});
+    });
   }
 
   render() {
@@ -31,7 +39,7 @@ class App extends Component {
           <h1>Store locator</h1>
           <SearchBar searchFunc={this.search} inputRef={this.searchInput}/>
         </header>
-        <SearchResults results={this.state.results}/>
+        <SearchResults results={this.state.results} isLoading={this.state.isLoading}/>
       </div>
     );
   }
--- src/components/searchresults.js
+++ src/components/searchresults.js
@@ -3,7 +3,10 @@ import React from 'react';
 import '../css/searchresults.css';
 import SearchResult from './searchresult'
 
-export default function SearchResults({results=[]}) {
+export default function SearchResults({results=[], isLoading=false}) {
+	if (isLoading) {
+		return (<p>Loading...</p>);
+	}
 	return (
 		<ul className="search-results">
 			{results.map((store, i) => <SearchResult key={i} store={store}/>)}
